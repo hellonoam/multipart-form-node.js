67fb738602f7bcadb060cb8626f54d524ad0ffac
Noam Szpiro <hellonoam@gmail.com>
2011-01-28 15:22:54 +0000

    soundcloud app

---
+++ app.js
@@ -0,0 +1,69 @@
+require.paths.unshift(__dirname + "/hello");
+
+var form     = require("./lib/connect-form"),
+	express  = require("express"),
+	fs = require("fs"),
+	util = require("util");
+
+var app = express.createServer( form({ keepExtensions: true }) );
+
+app.configure(function(){
+	app.use(express.methodOverride());
+	app.use(express.bodyDecoder());
+	app.use(app.router);
+	app.use(express.staticProvider(__dirname + "/public"));
+});
+
+var percentComplete = {};
+
+app.get("/", index);
+app.post("/", index);
+function index(req, res) {
+	res.writeHead(200, {"content-type": "text/html"});
+	var rs = fs.createReadStream("./public/index.html");
+	util.pump(rs, res);
+}
+
+app.get("/text", function(req, res) {
+	console.log("got text: " + req.param("text"));
+});
+
+app.post("/upload", function(req, res, next) {
+	if (!req.form)
+		return;
+	var session = req.param("session");
+	percentComplete[session] = 0;
+	console.log("got upload request from session " + session);
+	req.form.uploadDir = "./public/uploads/";
+	req.form.complete(function(err, fields, files){
+		if (err) {
+			next(err);
+		} else if (files.file) {
+			var path = files.file.path;
+			console.log("\nuploaded %s to %s",  files.file.filename, path);
+			res.writeHead(200, {"Content-Type": "text/plain"});
+			res.send("<div class='output'>" + path.substr(path.indexOf("/"))
+				   + "</div>");
+			res.end();
+		}
+	});
+
+	req.form.on("progress", function(bytesReceived, bytesExpected){
+		var percent = (bytesReceived / bytesExpected * 100) | 0;
+		if (percent != percentComplete[session]) {
+			process.stdout.write("Uploading: %" + percent + "\r");
+			percentComplete[session] = percent;
+		}
+	});
+	
+});
+
+app.get("/percent", function(req, res) {
+	var session = req.param("session");
+	console.log("got percent request from session " + session);
+	res.writeHead(200, {});
+	res.end("" + percentComplete[session]);
+});
+
+app.listen(3000);
+console.log("Express app started on port 3000");
---
+++ public/js/soundCloud.js
@@ -0,0 +1,49 @@
+$(document).ready(function() { init(); });
+function init() {
+
+	$(".submit").click(function(event) {
+		if ($("input[type=file]").val() == "") {
+			alert("you forgot to choose a file");
+			return;
+		}
+		var rand = Math.random();
+		$("form").attr("action", "/upload?session=" + rand);
+		updatePercent(rand);
+	});
+}
+
+function updatePercent(uploadId) {
+	console.log("update percent was called")
+	$.ajax({
+		url: "/percent",
+		data: { session: uploadId },
+		type: "GET",
+		success: function(data){
+			if (data != "undefined")
+				$(".percent").text(data + "%");
+			if (data != "100")
+				setTimeout(function() { updatePercent(uploadId); }, 200);
+			else {
+				$(".percent").text("100%");
+				done();
+			}
+		}
+	});
+}
+
+function done() {
+	$.ajax({
+		url: "/text",
+		data: { text: $("input[type=text]").val() },
+		type: "GET"
+	});
+	$("form").hide();
+	var path = $("iframe").contents().find(".output").text() || "";
+	if (path == "") {
+		setTimeout(done, 1000);
+		return;
+	}
+	$(".box").html("<p class='path'> path to file: <a href='" + 
+		path.substring(0,path.indexOf("<")) +
+		"'>here</a> </p>");
+}
