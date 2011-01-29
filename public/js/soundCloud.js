f9a4cdd4c53eabbcaddf305ea83bf153d46f612b
Noam Szpiro <hellonoam@gmail.com>
2011-01-29 19:37:57 +0000

    added support for IE

--- public/js/soundCloud.js
+++ public/js/soundCloud.js
@@ -1,9 +1,5 @@
 $(document).ready(function() { init(); });
 function init() {
-	CFInstall.check({
-		mode: "overlay",
-		destination: "http://ec2-50-16-138-59.compute-1.amazonaws.com/"
-	});
 	$(".submit").click(function(event) {
 		if ($("input[type=file]").val() == "") {
 			alert("you forgot to choose a file");
@@ -16,11 +12,11 @@ function init() {
 }
 
 function updatePercent(uploadId) {
-	console.log("update percent was called")
 	$.ajax({
 		url: "/percent",
 		data: { session: uploadId },
 		type: "GET",
+		cache: false,
 		success: function(data){
 			if (data != "undefined")
 				$(".percent").text(data + "%");
@@ -38,10 +34,11 @@ function done() {
 	$.ajax({
 		url: "/text",
 		data: { text: $("input[type=text]").val() },
-		type: "GET"
+		type: "GET",
+		cache: false
 	});
 	$("form").hide();
-	var path = $("iframe").contents().find(".output").text() || "";
+	var path = $("#frame").contents().find(".output").text() || "";
 	if (path == "") {
 		setTimeout(done, 1000);
 		return;
