939f79b046a5246b45fb8aad8995c939bbc78d5e
Noam Szpiro <hellonoam@gmail.com>
2011-01-31 09:38:58 +0000

    added comments

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
+//server that gets a multipart upload and has the ability to respond with the percent of
+//the upload complete
+
 require.paths.unshift(__dirname + "/hello");
 
 var form     = require("./lib/connect-form"),
 	express  = require("express"),
-	fs = require("fs"),
-	util = require("util");
+	fs       = require("fs"),
+	util     = require("util");
 
 var app = express.createServer( form({ keepExtensions: true }) );
 
+//configuration file
 app.configure(function(){
 	app.use(express.methodOverride());
 	app.use(express.bodyDecoder());
@@ -14,8 +18,10 @@ app.configure(function(){
 	app.use(express.staticProvider(__dirname + "/public"));
 });
 
+//holds the percent complete for all the active sessions
 var percentComplete = {};
 
+// '/' with get or post gets the index file in the public folder
 app.get("/", index);
 app.post("/", index);
 function index(req, res) {
@@ -24,12 +30,15 @@ function index(req, res) {
 	util.pump(rs, res);
 }
 
+//accepts text as a param
 app.get("/text", function(req, res) {
 	console.log("got text: " + req.param("text"));
 	res.writeHead(200, {"Content-Type": "text/plain"});
 	res.end();
 });
 
+//accepts a form with a file and a session id
+//uploads the file to path public/uploads
 app.post("/upload", function(req, res, next) {
 	if (!req.form)
 		return;
@@ -37,6 +46,7 @@ app.post("/upload", function(req, res, next) {
 	percentComplete[session] = 0;
 	console.log("got upload request from session " + session);
 	req.form.uploadDir = "./public/uploads/";
+	//when form has completed uploading
 	req.form.complete(function(err, fields, files){
 		if (err) {
 			next(err);
@@ -44,12 +54,14 @@ app.post("/upload", function(req, res, next) {
 			var path = files.file.path;
 			console.log("\nuploaded %s to %s",  files.file.filename, path);
 			res.writeHead(200, {"Content-Type": "text/plain"});
+			//responds with the path
 			res.send("<div class='output'>" + path.substr(path.indexOf("/"))
 				   + "</div>");
 			res.end();
 		}
 	});
 
+	//when uploading is in progress
 	req.form.on("progress", function(bytesReceived, bytesExpected){
 		var percent = (bytesReceived / bytesExpected * 100) | 0;
 		if (percent != percentComplete[session]) {
@@ -60,6 +72,7 @@ app.post("/upload", function(req, res, next) {
 	
 });
 
+//gets the percent complete of a certain session
 app.get("/percent", function(req, res) {
 	var session = req.param("session");
 	console.log("got percent request from session " + session);
--- public/js/soundCloud.js
+++ public/js/soundCloud.js
@@ -1,16 +1,22 @@
 $(document).ready(function() { init(); });
 function init() {
+	//adding a listener to the submit event
 	$(".submit").click(function(event) {
 		if ($("input[type=file]").val() == "") {
+			//in case a file wasn't chosen
 			alert("you forgot to choose a file");
 			return;
 		}
+		//the session id
 		var rand = Math.random();
 		$("form").attr("action", "/upload?session=" + rand);
+		//start asking for percent once the form is sent
 		updatePercent(rand);
 	});
 }
 
+//updates the percent field until it reaches 100 percent.
+//uploadId is the session id
 function updatePercent(uploadId) {
 	$.ajax({
 		url: "/percent",
@@ -21,8 +27,9 @@ function updatePercent(uploadId) {
 			if (data != "undefined")
 				$(".percent").text(data + "%");
 			if (data != "100")
+				//is not done continue after 200 miliseconds
 				setTimeout(function() { updatePercent(uploadId); }, 200);
-			else {
+			else { //done with upload
 				$(".percent").text("100%");
 				done();
 			}
@@ -30,19 +37,25 @@ function updatePercent(uploadId) {
 	});
 }
 
+//sends the text field and shows path to uploaded file
 function done() {
+	//sending the text field
 	$.ajax({
 		url: "/text",
 		data: { text: $("input[type=text]").val() },
 		type: "GET",
 		cache: false
 	});
+	//hiding form
 	$("form").hide();
+	//finding the path which is in the hidden iframe since the result was sent to there
 	var path = $("#frame").contents().find(".output").text() || "";
 	if (path == "") {
+		//if path wasn't updated yet
 		setTimeout(done, 1000);
 		return;
 	}
+	//presenting the path
 	$(".box").html("<p class='path'> path to file: <a href='" + 
 		path.substring(0,path.indexOf("<")) +
 		"'>here</a> </p>");
